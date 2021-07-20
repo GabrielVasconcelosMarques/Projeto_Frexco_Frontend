2c6613c6e0ec0d31a27ca1ef6652be8ddf64202a
GabrielVasconcelosMarques <gabrielvasco3431@gmail.com>
2021-07-20 19:21:28 -0300

    Select funcionando

--- src/pages/selectAll/SelectAll.js
+++ src/pages/selectAll/SelectAll.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import api from '../../services/services';
 import axios from 'axios';
+import './selectAll.css';
 
 
 export default class Produtos extends Component {
@@ -18,12 +19,32 @@ export default class Produtos extends Component {
         this.loadProdutos();
     };
 
-    loadProdutos = async (page = 1) => {
+    async loadProdutos(page = 1) {
         const response = await axios.get(`http://localhost:3005/sistema/produtos?page=${page}`);
         const {docs, ...produtosInfo} = response.data;
         
-
+        // setando os dados
         this.setState ({ produtos: docs, produtosInfo, page });
+    };
+
+    voltarPagina = () => {
+        const {page} = this.state;
+        if(page === 1) {
+            return
+        }
+
+        const pageNumber = page - 1;
+        this.loadProdutos(pageNumber);
+    }
+
+    avancarPagina = () => {
+        const {page, produtosInfo} = this.state;
+        if(page === produtosInfo.pages) {
+            return
+        }
+
+        const pageNumber = page + 1;
+        this.loadProdutos(pageNumber);
     }
 
     // renderizando todos os elementos retornados pelo backend
@@ -31,12 +52,20 @@ export default class Produtos extends Component {
         const { produtos, produtosInfo, page } = this.state;
         return (
             <div>
-                {this.state.produtos.map(produto => (
-                    <article key={produto._id}>
-                        <strong>{produto.nome}</strong>
-                        <p>{produto.valor}</p>
-                    </article>
-                ))}
+                <div className="main">
+                    {this.state.produtos.map(produto => (
+                        <article key={produto._id}>
+                            <strong>{produto.nome}</strong>
+                            <p>${produto.valor}</p>
+                            <p>Qtde: {produto.quantidade}</p>
+                            <p><Link to={`/produtos/${produto._id}`}>Ver produto</Link></p>
+                        </article>
+                    ))}
+                </div>
+                <div className="botoes">
+                        <button disabled={page===1} onClick={this.voltarPagina}>Anterior</button>
+                        <button disabled={page===produtosInfo.pages} onClick={this.avancarPagina}>Próxima</button>
+                    </div>
             </div>
         )
     }
